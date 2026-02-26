08b0f85052c0531538865b3abab8b54b38dab888
Boris <102739976+l2ggy@users.noreply.github.com>
2026-02-26 04:58:15 -0500

    Add iOS root icon rewrites and apple-touch precomposed link

--- src/worker.js
+++ src/worker.js
@@ -1,3 +1,19 @@
+const ROOT_ICON_REWRITES = {
+  "/favicon.ico": "/icons/favicon.ico",
+  "/apple-touch-icon.png": "/icons/apple-touch-icon.png",
+  "/apple-touch-icon-precomposed.png": "/icons/apple-touch-icon.png",
+};
+
 export default {
-  fetch: (request, env) => env.ASSETS.fetch(request),
+  fetch(request, env) {
+    const url = new URL(request.url);
+    const rewrittenPath = ROOT_ICON_REWRITES[url.pathname];
+
+    if (rewrittenPath) {
+      url.pathname = rewrittenPath;
+      return env.ASSETS.fetch(new Request(url.toString(), request));
+    }
+
+    return env.ASSETS.fetch(request);
+  },
 };
